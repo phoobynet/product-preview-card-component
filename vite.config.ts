0a6607bce06d383e7ca769f6bb635270e701a888
Glenn <phoobynet@users.noreply.github.com>
2022-11-18 10:49:16 +0000

    deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/fm-product-preview-card-component/',
+  base: '/product-preview-card-component/',
   plugins: [react()],
   css: {
     modules: {
